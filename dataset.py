1bf8e1bf3d5a39052b2b4e4aaa21411d62fb51bf
harusakueeic <harusaku0412@g.ecc.u-tokyo.ac.jp>
2021-11-11 14:05:28 +0900

    make DCGAN

---
+++ dataset.py
@@ -0,0 +1,31 @@
+import os
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+import glob
+import numpy as np
+class MyDataset(Dataset):
+	def __init__(self, category, root=None, train=True, transform=None):
+		self.root = root
+		self.transform = transform
+		mode = 'train' if train else 'test'
+		# TODO class辞書の定義
+		self.category = category
+		# TODO 画像パスとそのラベルのセットをself.all_dataに入れる
+		self.all_data = []
+
+		data = np.load(self.root+ self.category + ".npy")
+		self.data = [[data_i.reshape(28,28),self.category] for data_i in data]
+
+	def __len__(self):
+		#データセットの数を返す関数
+		return len(self.data)
+		
+	def __getitem__(self, idx):
+		# TODO 画像とラベルの読み取り
+		#self.all_dataを用いる
+		data_img = Image.fromarray(self.data[idx][0]).convert("L")
+		if self.transform is not None:
+			data_img = self.transform(data_img)
+		label = self.data[idx][1]
+		return [data_img, label]
---
+++ utils.py
@@ -0,0 +1,81 @@
+from genericpath import exists
+import torch
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+import math
+import itertools
+import imageio
+import natsort
+from glob import glob
+import os
+
+from dataset import MyDataset
+
+#######################################
+## 使うカテゴリの名前を代入          ##
+#######################################
+category = "book"
+
+def get_data_loader(batch_size):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.1307, ), std=(0.3081, ))])
+    train_dataset = MyDataset(category,"./raw_data/", transform=transform)
+
+    # Data Loader
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    return train_loader
+
+def generate_images(epoch, path, fixed_noise, num_test_samples, netG, device, use_fixed=False):
+    z = torch.randn(num_test_samples, 100, 1, 1, device=device)
+    size_figure_grid = int(math.sqrt(num_test_samples))
+    title = None
+    path += (category + "/")
+  
+    if use_fixed:
+        generated_fake_images = netG(fixed_noise)
+        path += 'fixed_noise/'
+        title = 'Fixed Noise'
+        os.makedirs(path, exist_ok=True)
+        
+    else:
+        generated_fake_images = netG(z)
+        path += 'variable_noise/'
+        title = 'Variable Noise'
+        os.makedirs(path, exist_ok=True)
+  
+    fig, ax = plt.subplots(size_figure_grid, size_figure_grid, figsize=(6,6))
+    for i, j in itertools.product(range(size_figure_grid), range(size_figure_grid)):
+        ax[i,j].get_xaxis().set_visible(False)
+        ax[i,j].get_yaxis().set_visible(False)
+    for k in range(num_test_samples):
+        i = k//4
+        j = k%4
+        ax[i,j].cla()
+        ax[i,j].imshow(generated_fake_images[k].data.cpu().numpy().reshape(28,28), cmap='Greys')
+    label = 'Epoch_{}'.format(epoch+1)
+
+    fig.text(0.5, 0.04, label, ha='center')
+    fig.suptitle(title)
+    fig.savefig(path+label+'.png')
+
+def save_gif(path, fps, fixed_noise=False):
+    path += category+"/"
+    if fixed_noise==True:
+        path += 'fixed_noise/'
+        os.makedirs(path, exist_ok=True)
+    else:
+        path += 'variable_noise/'
+        os.makedirs(path, exist_ok=True)
+    images = glob(path + '*.png')
+    images = natsort.natsorted(images)
+    gif = []
+
+    for image in images:
+        gif.append(imageio.imread(image))
+    os.makedirs("gif/", exist_ok=True)
+    imageio.mimsave("gif/"+category +'_animated.gif', gif, fps=fps)
+
+    
+
+    
